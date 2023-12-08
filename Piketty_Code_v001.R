a75f3f3fb811f63898193be4bbe5f02af85bb1eb
NicolasRoever <nicoroever@gmail.com>
2023-12-08 13:47:42 +0100

    Intermediate Version of plot code.

--- Piketty_Code_v001.R
+++ Piketty_Code_v001.R
@@ -15,35 +15,32 @@ years <- c(
   mean(1960,1970), 
   mean(1970,1980), 
   mean(1980,1990), 
-  mean(1990, 2000), 
-  mean(2000, 2010), 
-  mean(2010,2020)
+  mean(1990, 2020)
+
   
 )
 # Create a vector of estimated revenue values corresponding to the years
-revenue_values <- c(
-  10000,          # 1802-1830
-  100000,          # 1830-1860
-  500000,          # 1860-1880
-  2000000,         # 1880-1900
-  5000000,         # 1900-1910
-  10000000,        # 1910-1920
-  20000000,        # 1920-1930
-  40000000,        # 1930-1940
-  60000000,        # 1940-1950
-  90000000,        # 1950-1960
-  120000000,       # 1960-1970
-  150000000,       # 1970-1980
-  180000000,       # 1980-1990
-  210000000,       # 1990-2000
-  300000000,       # 2000-2010
-  400000000     # 2010-2020
+# Create a vector of estimated company value values corresponding to the years
+company_values <- c(
+  10000,  # 1802-1830
+  100000,  # 1830-1860
+  500000,  # 1860-1880
+  2000000,  # 1880-1900
+  5000000,  # 1900-1910
+  10000000,  # 1910-1920
+  20000000,  # 1920-1930
+  40000000,  # 1930-1940
+  60000000,  # 1940-1950
+  90000000,  # 1950-1960
+  120000000,  # 1960-1970
+  300000000,  # 1970-1980
+  700000000,  # 1980-1990
+  738100000  # 1990-2020
 )
-
 # Combine years and revenue values into a data frame
-revenue_data <- data.frame(
+value_data <- data.frame(
   Year = years,
-  Revenue = revenue_values
+  Company_Value = company_values
 )
 
 
@@ -51,12 +48,12 @@ revenue_data <- data.frame(
 
 
 
-ggplot(revenue_data[revenue_data$Year >= 1800, ], aes(x = Year, y = Revenue/1000)) +
+ggplot(value_data[value_data$Year >= 1800, ], aes(x = Year, y = Company_Value/10000000)) +
   geom_line() +
   labs(
-    title = "DuPont Revenue Growth over Time",
+    title = "DuPont Company Value over Time (in Billion Dollars)",
     x = "Year",
-    y = "Revenue (in billions)"
+    y = "Market Value"
   ) + # Add annotations for significant events
 #annotate("text", x = 1865, y = 1000, label = "Civil War", size = 4) +
 #annotate("text", x = 1910, y = 2000, label = "World War I", size = 4) +
@@ -67,5 +64,5 @@ ggplot(revenue_data[revenue_data$Year >= 1800, ], aes(x = Year, y = Revenue/1000
 
 
 #Average DuPont Return
-(400000000 /1000)^(1/200) - 1
+(  738100000 /10000)^(1/200) - 1
 
